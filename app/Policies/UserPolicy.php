65c6554880cb3c988f59fd123e74e3fe6287fc9f
SistemasSJS <sistemas_sjs@hotmail.com>
2025-05-29 02:25:38 -0700

    POLICYS

---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class UserPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return $user->isUserAdmin();
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, User $user_): bool
+    {
+        return $user->isUserAdmin();
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->isUserAdmin();
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, User $user_): bool
+    {
+        return $user->isUserAdmin();
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, User $user_): bool
+    {
+        return $user->isSuperAdmin(); // Solo super admin
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, User $user_): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, User $user_): bool
+    {
+        return false;
+    }
+}
