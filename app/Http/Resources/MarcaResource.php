2f003ae6d29173ee234ac9e8268ea51e9fa6d232
Geronimo Montes <sistemas_sjs@hotmail.com>
2025-08-23 10:04:58 -0700

    catalogos con tawbla

--- app/Http/Resources/MarcaResource.php
+++ app/Http/Resources/MarcaResource.php
@@ -16,9 +16,11 @@ class MarcaResource extends JsonResource
     {
         return [
             'id'         => $this->id,
-            'nombre'     => $this->nombre,
             'logo'       => asset('storage/' . $this->logo),
-            // 'estatus'    => $this->estatus,
+            'nombre'     => $this->nombre,
+            'descripcion'     => $this->descripcion,
+            'productos_count' => $this->productos_count, // 👈 aquí lo expones
+            'estatus'    => $this->estatus,
             // 'created_at' => $this->created_at?->toDateTimeString(),
             // 'updated_at' => $this->updated_at?->toDateTimeString(),
         ];
