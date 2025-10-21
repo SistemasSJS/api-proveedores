8767fea19b368d800bea33385f423ac1a988392d
SistemasSJS <sistemas_sjs@hotmail.com>
2025-10-21 06:02:41 -0700

    notifiacionwes reverb

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -11,7 +11,7 @@ return [
     | framework when an event needs to be broadcast. You may set this to
     | any of the connections defined in the "connections" array below.
     |
-    | Supported: "pusher", "ably", "redis", "log", "null"
+    | Supported: "reverb", "pusher", "ably", "redis", "log", "null"
     |
     */
 
@@ -23,12 +23,26 @@ return [
     |--------------------------------------------------------------------------
     |
     | Here you may define all of the broadcast connections that will be used
-    | to broadcast events to other systems or over websockets. Samples of
+    | to broadcast events to other systems or over WebSockets. Samples of
     | each available type of connection are provided inside this array.
     |
     */
 
     'connections' => [
+        'reverb' => [
+            'driver' => 'pusher',
+            'key' => env('REVERB_APP_KEY'),
+            'secret' => env('REVERB_APP_SECRET'),
+            'app_id' => env('REVERB_APP_ID'),
+            'options' => [
+                'cluster' => env('PUSHER_APP_CLUSTER', 'mt1'), // necesario para Pusher JS
+                'host' => env('REVERB_HOST', '127.0.0.1'),
+                'port' => env('REVERB_PORT', 8080),
+                'scheme' => env('REVERB_SCHEME', 'http'),     // http en lugar de https
+                'useTLS' => false,
+            ],
+        ],
+
 
         'pusher' => [
             'driver' => 'pusher',
@@ -36,11 +50,12 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'host' => env('PUSHER_HOST') ?: 'api-' . env('PUSHER_APP_CLUSTER', 'mt1') . '.pusherapp.com',
+                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'host' => env('PUSHER_HOST') ?: 'api-' . env('PUSHER_APP_CLUSTER', 'mt1') . '.pusher.com',
                 'port' => env('PUSHER_PORT', 443),
-                'scheme' => env('PUSHER_SCHEME', 'https'),
+                'scheme' => env('PUSHER_SCHEME', 'http'),
                 'encrypted' => true,
-                'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
+                'useTLS' => env('PUSHER_SCHEME', 'http') === 'http',
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
@@ -50,11 +65,6 @@ return [
         'ably' => [
             'driver' => 'ably',
             'key' => env('ABLY_KEY'),
-        ],
-
-        'redis' => [
-            'driver' => 'redis',
-            'connection' => 'default',
         ],
 
         'log' => [
--- routes/segmented/public.php
+++ routes/segmented/public.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\UnidadMedidaController;
 use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\MarcaController;
 use App\Http\Controllers\PedidoController;
+use App\Events\TestEvent;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,26 +25,29 @@ use App\Models\User;
 use App\Notifications\PushNotification;
 
 Route::get('status', function () {
-    // Buscar el usuario con ID 3
-    $user = User::find(13);
 
-    if (!$user) {
-        return response()->json([
-            'status' => 'error',
-            'message' => 'Usuario no encontrado',
-        ], 404);
-    }
+    // Lanzar evento con un mensaje
+    event(new TestEvent("¡Hola desde Laravel!"));
+    // Buscar el usuario con ID 3
+    // $user = User::find(14);
+
+    // if (!$user) {
+    //     return response()->json([
+    //         'status' => 'error',
+    //         'message' => 'Usuario no encontrado',
+    //     ], 404);
+    // }
 
     // Crear la notificación
-    $notification = new PushNotification(
-        'Título de prueba',
-        'Este es un mensaje de prueba',
-        'info',
-        ['extra' => 'datos opcionales']
-    );
+    // $notification = new PushNotification(
+    //     'Título de prueba',
+    //     'Este es un mensaje de prueba',
+    //     'info',
+    //     ['extra' => 'datos opcionales']
+    // );
 
     // Enviar la notificación
-    $user->notify($notification);
+    // $user->notify($notification);
 
     return response()->json([
         'status' => 'ok',
