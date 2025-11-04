eb1db5e0e8b06af62a123946584fcbafb6088f86
Geronimo Montes <sistemas_sjs@hotmail.com>
2025-11-03 17:33:14 -0700

    cout sp para siguiente

--- app/Services/InterApiService.php
+++ app/Services/InterApiService.php
@@ -23,11 +23,11 @@ class InterApiService
   {
     try {
       $payload = [
-        'sp_id' => 1,
+        'sp_id' => $sp->id,
         'company' => 14,
         'obra' => 1,
         'message' => 'Nueva Solicitud de compra',
-        'user_id' => 1,
+        'user_id' => 75,
       ];
 
       // Enviar a API Proveedores con ApiKey header
