ccc3b9ab693553fa4178cc8dc29a62f1f58e6bae
Geronimo Montes <sistemas_sjs@hotmail.com>
2025-12-30 18:52:18 -0700

    sp por validor conteo por emrpesa para usuarios directrivos #1

--- app/Http/Controllers/ConstruccSolicitudPagoController.php
+++ app/Http/Controllers/ConstruccSolicitudPagoController.php
@@ -1376,7 +1376,7 @@ class ConstruccSolicitudPagoController extends Controller
         // 👉 Construcción de la query
         $query = SolicitudPago::on('mysql5')
             ->where('verificada', false)
-            ->where('usuario_id', '!=', $usuarioId) // excluye el usuario
+            // ->where('usuario_id', '!=', $usuarioId) // excluye el usuario
             ->where('empresa_construcc_id', $empresaConstruccId)
             ->where('estado_solicitud', EstadoSP::PENDIENTE->value)
             ->filter($filters);
