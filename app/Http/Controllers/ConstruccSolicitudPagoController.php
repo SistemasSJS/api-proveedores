22cc5303fcdee90452fd4eb7e9d2e9e87ffd8f11
Geronimo Montes <sistemas_sjs@hotmail.com>
2026-02-14 10:42:50 -0700

    sin filtro de verificada en index spp.

--- app/Http/Controllers/ConstruccSolicitudPagoController.php
+++ app/Http/Controllers/ConstruccSolicitudPagoController.php
@@ -79,7 +79,7 @@ class ConstruccSolicitudPagoController extends Controller
         $filters = $request->only(SolicitudPago::getFilters());
         $sortBy = $request->input('sort_by', 'created_at');
         $order = $request->input('order', 'desc');
-        // $perPage = $request->input('per_page', 10000);
+        $perPage = $request->input('per_page', 10000);
 
         $query = SolicitudPago::query()
             ->with(SolicitudPago::eagerLodable())
