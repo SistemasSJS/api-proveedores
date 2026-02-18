f99b503f9e893940f51cc173d5a89417373ca40f
Geronimo Montes <sistemas_sjs@hotmail.com>
2026-02-18 13:05:35 -0700

    feat: Exclude proveedores with tipo_alta = 2 from the query

--- app/Http/Controllers/ConstruccSolicitudPagoController.php
+++ app/Http/Controllers/ConstruccSolicitudPagoController.php
@@ -1067,7 +1067,7 @@ class ConstruccSolicitudPagoController extends Controller
         $usuarioConstruccId = $request->input('usuario_construcc_id');
 
         $proveedores = \App\Models\Proveedor::query()
-            ->where('tipo_actividad_id', 1) // Solo proveedores de tipo "Materiales"
+            ->where('tipo_alta', '!=', 2)   // 🔥 Excluir tipo_alta = 2
             ->whereHas('empresasConstrucc', function ($q) use ($empresaId, $usuarioConstruccId) {
                 $q->where('empresa_construcc_id', $empresaId);
 
