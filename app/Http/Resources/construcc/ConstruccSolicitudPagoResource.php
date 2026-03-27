ad6c6b39f7d6013eec898bc593d60920a5e869a6
Geronimo Montes <sistemas_sjs@hotmail.com>
2026-03-27 09:51:28 -0700

    repuesta sp marcar como validada con monto

--- app/Http/Resources/construcc/ConstruccSolicitudPagoResource.php
+++ app/Http/Resources/construcc/ConstruccSolicitudPagoResource.php
@@ -112,6 +112,12 @@ class ConstruccSolicitudPagoResource extends JsonResource
             'motivo_autorizacion_parcial' => $this->motivo_autorizacion_parcial,
             'fecha_autorizacion_parcial' => $this->fecha_autorizacion_parcial,
             // ?->format('Y-m-d H:i:s'),
+            'validacion_con_monto' => [
+                'monto' => (float) $this->validacion_monto,
+                'usuario_id' => $this->validacion_usuario_id,
+                'fecha' => $this->validacion_fecha,
+                'motivo' => $this->validacion_motivo,
+            ]
         ];
     }
 }
